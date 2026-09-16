8d8cc5d4f5d34bf2d79746f7d2077059a06b8472
Olmachai <287800540+Olmachai@users.noreply.github.com>
2026-09-16 23:11:06 +0300

    Improved fastfetch, bash and starship configs

--- home/bash/bash.nix
+++ home/bash/bash.nix
@@ -1,9 +1,20 @@
-{ pkgs, config, ... }: let
+{ pkgs, config, ... }: {
   
-  bashrcpath = "${config.home.homeDirectory}/nixos-config/home/bash/bashrc";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "eza --icons -l";
+      ls = "eza --icons";
+      wgdown = "sudo wg-quick down ~/.config/wg-quick/wg0.conf";
+      wgup = "sudo wg-quick up ~/.config/wg-quick/wg0.conf";
+      ".." = "cd ..";
+      cdrebuild = 
+        ''
+          cd ~/nixos-config
+          sudo nixos-rebuild switch --flake .
+        '';
+      rebuild = "sudo nixos-rebuild switch --flake .";
+    };
+  };
 
-  in
-  {
-
-  home.file.".bashrc".source = config.lib.file.mkOutOfStoreSymlink bashrcpath;
 }
